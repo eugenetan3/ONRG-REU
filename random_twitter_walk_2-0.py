f2dff7cb64fae21fe7ed6b5140f987249f1ac7ba
eugenetan3 <etanshs0@gmail.com>
2020-10-08 15:43:08 -0700

    up

--- random_twitter_walk_2-0.py
+++ random_twitter_walk_2-0.py
@@ -15,25 +15,25 @@ import random
 
 current_date = time.strftime("%Y%m%d")
 
-base_path='/home/eugenet/WorkingDir/data/top_accounts_%s/' % current_date
+base_path='/home/eugenet/WorkingDir/random_walk_data/random_walk_%s_' % current_date
 
 def random_twitter_walk(random_walk_file, token_file, date, seed_id):
     all_tokens = []
     tokens = open(token_file)
     for line in tokens:
         all_tokens.append(line.strip().split()[:])
+
+    for i in all_tokens:
+        print(i)
+
     last_timeout = time.time()
-    token_counter = 1
-    print ('Beginning random walk....')
-    print (twython.__version__)
-    #print ('Base path: ' + base_path)
-    #print ('Random Walk File: ' + random_walk_file)
-    #print ('Date: ' + date)
-    #print ('Seed_ID: ' + seed_id)
+    token_counter = 0
+
+
     user_count = 0
     random_walk_ids = []
     step = seed_id
-    while (len(random_walk_ids) < 2):
+    while (len(random_walk_ids) < 100):
         print ('Current number of profiles walked so far: %d' % user_count)
         user_count = user_count + 1
         have_path = False
@@ -52,7 +52,7 @@ def random_twitter_walk(random_walk_file, token_file, date, seed_id):
                     #print(search2)
                     friend_ids.extend(search['ids'])
                     friend_ids.extend(search2['ids'])
-                    #print("How many people: " + len(friend_ids))
+                    print("How many people: " + len(friend_ids))
                     next_cursor = search2["next_cursor"]
                 except twython.TwythonRateLimitError:
                     print ("Token number is %d" % (token_counter % len(all_tokens)))
@@ -63,7 +63,7 @@ def random_twitter_walk(random_walk_file, token_file, date, seed_id):
                     break
                 
                 if token_counter % len(all_tokens) == 0:
-                    sleep_amount = 900
+                    sleep_amount = 5
                     last_timeout = time.time()
                     print("Tokens are exhausted, sleeping for %f minutes!" % (sleep_amount / 60.0))
                     time.sleep(sleep_amount)
@@ -85,7 +85,7 @@ def random_twitter_walk(random_walk_file, token_file, date, seed_id):
                 else:
                     step = random_walk_ids[-back_step]
     print(len(friend_ids))        
-    walk_file = open("random_walk_files.txt", "a")
+    walk_file = open(base_path + seed_id, "w")
     print('Writing results of random walk to %s' % random_walk_file)
     for user_id in random_walk_ids:
         followers_count = None
@@ -117,7 +117,7 @@ def random_twitter_walk(random_walk_file, token_file, date, seed_id):
         if ((user_id is not None) and (followers_count is not None) and (screen_name is not None)):
             walk_file.write('%s,%s,%s\n' % (str(user_id), str(screen_name), str(followers_count)))
         if token_counter & len(all_tokens) == 0:
-            sleep_amount = 900
+            sleep_amount = 5
             last_timeout = time.time()
             print("All tokens are exhausted, sleeping for %f minutes" % (sleep_amount / 60.0))
             time.sleep(sleep_amount)
@@ -127,6 +127,7 @@ def random_twitter_walk(random_walk_file, token_file, date, seed_id):
     print("Random walk successfully finished")
 
 if __name__ == '__main__':
-    random_twitter_walk("/home/eugenet/WorkingDir/data/top_accounts_20200719/random_walk_users", "token.txt", current_date, "1280557030562869248")
-    #random_twitter_walk(base_path + "random_walk_users", "token.txt", current_date, "25073877")
+    #random_twitter_walk("/home/eugenet/WorkingDir/data/top_accounts_20200719/random_walk_users", "token.txt", current_date, "1280557030562869248")
+    #random_twitter_walk(base_path + "106387406", "token.txt", current_date, "106387406")
+    random_twitter_walk(base_path + "358996059", "token.txt", current_date, "358996059")
     #  SEEDS ARE ELITES FOR RANDOM
